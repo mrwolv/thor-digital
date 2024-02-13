1358ac1390e9dc2a832e013132488f092f645cf6
depu <87559657+mrwolv@users.noreply.github.com>
2024-02-13 14:06:40 +0530

    working

--- FrontEnd/src/components/Capabilities.jsx
+++ FrontEnd/src/components/Capabilities.jsx
@@ -3,8 +3,13 @@ import gsap from "gsap";
 import { useEffect } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import SplitType from "split-type";
+import UseFetch from "../hooks/UseFetch";
 
-const Capabilities = ({ data }) => {
+const Capabilities = () => {
+
+  const {data} = UseFetch('http://localhost:1337/api/capabilitie?populate=*')
+ 
+
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     const ourText = new SplitType(".our-text", { types: "chars" });
@@ -31,8 +36,8 @@ const Capabilities = ({ data }) => {
 
     gsap.to(".container", {
       scrollTrigger: {
-        pin: ".container",
-        end: "+=750",
+        pin: '.container',
+        end: "+=600",
         pinSpacing: true,
         scrub: true,
       },
@@ -50,13 +55,14 @@ const Capabilities = ({ data }) => {
           y: 0,
           opacity: 1,
           duration: 1.2,
-          delay:1.4*index,
-          ease: "power2.inOut",
+          delay:1.8*index,
+          ease: "power3.inOut",
           scrollTrigger: {
             trigger: box,
             start: "top bottom", // Adjust start point for each box animation
             end: "bottom -200%", // Adjust end point for each box animation
             toggleActions: "play none none none",
+            
           },
         }
       );
@@ -64,20 +70,20 @@ const Capabilities = ({ data }) => {
   }, [data?.Capabilities]);
 
   return (
-    <div className="bg-[#562ABD] md:px-20 md:py-24 py-4 px-6 ">
+    <div className="bg-[#562ABD] md:px-20 md:py-24 py-4 px-6  ">
       <div>
         <h1 className="text-[2.5rem] md:text-[3rem] font-extrabold text-white our-text clip-your-needful-style   ">
           Capabilities
         </h1>
-        <div className="grid grid-flow-row md:grid md:grid-cols-2 gap-10 py-10  container  ">
-          {data && data.Capabilities.map((item) => (
+        <div className="grid grid-flow-row md:grid md:grid-cols-2 gap-10 py-10  container   ">
+          {data && data?.Capabilities.map((item) => (
             <div
               className="flex flex-col  justify-between gap-7 text-white box"
               key={item.capNum}
             >
-              <span className="text-[1.2rem] font-bold">0{item.capNum}</span>
-              <h1 className="text-[1.6rem] font-bold">{item.capTitle}</h1>
-              <span className="text-[1rem]">{item.capDescription}</span>
+              <span className="text-[1.2rem] font-bold">0{item.id}</span>
+              <h1 className="text-[1.6rem] font-bold">{item.Title}</h1>
+              <span className="text-[1rem]">{item.Description}</span>
               <hr className="h-2" />
             </div>
           ))}
